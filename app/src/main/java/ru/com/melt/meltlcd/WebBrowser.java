f5cd08c921aa7e17fc15356cc9c41ef672b9e9d1
pashtetez <pashtetez>
2018-08-31 01:54:57 +0300

    java code reorganized

--- app/src/main/java/ru/com/melt/meltlcd/WebBrowser.java
+++ app/src/main/java/ru/com/melt/meltlcd/WebBrowser.java
@@ -143,25 +143,92 @@ public class WebBrowser extends AppCompatActivity {
             }
         } catch (JSONException e) {
             e.printStackTrace();
-        } finally {
-            return r.toString();
+        }
+        return r.toString();
+    }
+
+    private class CustomBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            if (url.endsWith("my_live_loading_script.js")) {
+                Log.e("mymessage", url);
+                int index = url.lastIndexOf('/');
+                webView.loadUrl(url.substring(0, index) + "/swap1.txt");
+            } else if (Uri.parse(url).getScheme().equals("file")) {
+                webView.loadUrl(url);
+            } else {
+                // If the URI is not pointing to a local file, open with an ACTION_VIEW Intent
+                webView.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+            return true; // in both cases we handle the link manually
+        }
+
+        @Override
+        public WebResourceResponse shouldInterceptRequest(final WebView view, String url) {
+            if (url.endsWith("my_live_loading_script.js")) {
+                Log.e("mymessage1", url);
+                return getCssWebResourceResponseFromString();
+            } else {
+                Log.e("mymessage", url);
+                return assetServer.shouldInterceptRequest(url);
+            }
+        }
+
+        private WebResourceResponse getCssWebResourceResponseFromString() {
+            //String exampleString = "b = \"wow\";";
+
+            try {
+                //URLConnection cn = new URL("http://192.168.1.5:8000/my_live_loading_script.js").openConnection();
+                Log.e("mymessage", "hiu");
+                URL url = new URL("http://www.melt.com.ru/pdf/swap1.txt");
+                String exampleString = "";
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, "cp1251"));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\r\n");
+                    }
+                    exampleString = sb.toString();
+                    exampleString = "data = " + getJson(exampleString);
+                } finally {
+                    urlConnection.disconnect();
+                }
+                boolean success = GetCacheDir.writeAllCachedText("myCacheFile.txt", exampleString);
+                InputStream stream = new ByteArrayInputStream(exampleString.getBytes(Charset.forName("UTF-8")));
+                return getUtf8EncodedCssWebResourceResponse(stream);
+            } catch (IOException e) {
+                String exampleString = GetCacheDir.readAllCachedText("myCacheFile.txt");
+                InputStream stream = new ByteArrayInputStream(exampleString.getBytes(Charset.forName("UTF-8")));
+                return getUtf8EncodedCssWebResourceResponse(stream);
+            }
+
+
+        }
+
+        /**
+         * Return WebResourceResponse with CSS markup from an asset (e.g. "assets/style.css").
+         */
+        private WebResourceResponse getCssWebResourceResponseFromAsset() {
+            try {
+                return getUtf8EncodedCssWebResourceResponse(getAssets().open("style.css"));
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        /**
+         * Return WebResourceResponse with CSS markup from a raw resource (e.g. "raw/style.css").
+         */
+        //            private WebResourceResponse getCssWebResourceResponseFromRawResource() {
+        //                return getUtf8EncodedCssWebResourceResponse(getResources().openRawResource(R.raw.style));
+        //            }
+        private WebResourceResponse getUtf8EncodedCssWebResourceResponse(InputStream data) {
+            return new WebResourceResponse("text/css", "UTF-8", data);
         }
     }
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url.startsWith("tel:") || url.startsWith("sms:") || url.startsWith("smsto:") || url.startsWith("mailto:") || url.startsWith("mms:") || url.startsWith("mmsto:") || url.startsWith("market:")) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-                return true;
-            }
-            else {
-                view.loadUrl(url);
-                return true;
-            }
-        }
-    }
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_MENU) {
             WebView myWebView = findViewById(R.id.webview);
@@ -184,18 +251,15 @@ public class WebBrowser extends AppCompatActivity {
 
     public class WebAppInterface {
         Context mContext;
-
         /** Instantiate the interface and set the context */
         WebAppInterface(Context c) {
             mContext = c;
         }
-
         /** Show a toast from the web page */
         @JavascriptInterface
         public void showToast(String toast) {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
-
         @JavascriptInterface
         public void exit() {
             Intent intent = new Intent(Intent.ACTION_MAIN);
@@ -205,7 +269,6 @@ public class WebBrowser extends AppCompatActivity {
             finish();
             System.exit(0);
         }
-
     }
 
     WebViewLocalServer assetServer = null;
@@ -214,129 +277,31 @@ public class WebBrowser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         context = this;
-
-        Window window = this.getWindow();
-
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        ///contextwindow.setStatusBarColor(Color.parseColor("#009900"));//High api level
-
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { //prettify app if API allows
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(Color.parseColor("#009900"));//High api level
+        }
         //int numDeletedFiles = clearCacheFolder(context.getCacheDir(), 0);
+        //boolean success = GetCacheDirExample.writeAllCachedText( "myCacheFile.txt", textToCache);
+        //https://stackoverflow.com/questions/44979340/html-link-to-local-file-in-webview-with-target-api-24
         assetServer = new WebViewLocalServer(context);
         WebViewLocalServer.AssetHostingDetails details = assetServer.hostAssets("");
         indexUrl = details.getHttpPrefix().buildUpon().appendPath("index.html").toString();
         //indexUrl = "http://192.168.1.5:8001/index.html";
-        //indexUrl = "http://192.168.2.72:8001/index.html";
-        indexUrl = "https://www.google.ru/";
         setContentView(R.layout.activity_web_browser);
         WebView myWebView = findViewById(R.id.webview);
         myWebView.setWebChromeClient(new WebChromeClient());
-
-        Log.i("mymessage","mymessage");
-        String textToCache = "b = \"wow1\";";
-
-//        boolean success = GetCacheDirExample.writeAllCachedText( "myCacheFile.txt", textToCache);
-
-        //https://stackoverflow.com/questions/44979340/html-link-to-local-file-in-webview-with-target-api-24
-        myWebView.setWebViewClient(new WebViewClient() {
-                                       @Override
-                                       public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-                                           if (url.endsWith("my_live_loading_script.js")) {
-                                               Log.e("mymessage", url);
-                                               int index = url.lastIndexOf('/');
-                                               webView.loadUrl(url.substring(0, index) + "/swap1.txt");
-                                           } else if (Uri.parse(url).getScheme().equals("file")) {
-                                               webView.loadUrl(url);
-                                           } else {
-                                               // If the URI is not pointing to a local file, open with an ACTION_VIEW Intent
-                                               webView.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                                           }
-                                           return true; // in both cases we handle the link manually
-                                       }
-
-                                       @Override
-                                       public WebResourceResponse shouldInterceptRequest(final WebView view, String url) {
-                                           if (url.endsWith("my_live_loading_script.js")) {
-                                               Log.e("mymessage1", url);
-                                               return getCssWebResourceResponseFromString();
-                                           } else {
-                                               Log.e("mymessage", url);
-                                               return assetServer.shouldInterceptRequest(url);
-                                           }
-                                       }
-
-                                       private WebResourceResponse getCssWebResourceResponseFromString() {
-                                           //String exampleString = "b = \"wow\";";
-
-                                           try {
-                                               //URLConnection cn = new URL("http://192.168.1.5:8000/my_live_loading_script.js").openConnection();
-                                               Log.e("mymessage", "hiu");
-                                               URL url = new URL("http://www.melt.com.ru/pdf/swap1.txt");
-                                               String exampleString = "";
-                                               HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                                               try {
-                                                   InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                                                   BufferedReader br = new BufferedReader(new InputStreamReader(in, "cp1251"));
-                                                   StringBuilder sb = new StringBuilder();
-                                                   String line;
-                                                   while ((line = br.readLine()) != null) {
-                                                       sb.append(line + "\r\n");
-                                                   }
-                                                   exampleString = sb.toString();
-                                                   exampleString = "data = " + getJson(exampleString);
-                                               } finally {
-                                                   urlConnection.disconnect();
-                                               }
-                                               boolean success = GetCacheDir.writeAllCachedText("myCacheFile.txt", exampleString);
-                                               InputStream stream = new ByteArrayInputStream(exampleString.getBytes(Charset.forName("UTF-8")));
-                                               return getUtf8EncodedCssWebResourceResponse(stream);
-                                           } catch (IOException e) {
-                                               String exampleString = GetCacheDir.readAllCachedText("myCacheFile.txt");
-                                               InputStream stream = new ByteArrayInputStream(exampleString.getBytes(Charset.forName("UTF-8")));
-                                               return getUtf8EncodedCssWebResourceResponse(stream);
-                                           }
-
-
-                                       }
-
-                                        /**
-                                         * Return WebResourceResponse with CSS markup from an asset (e.g. "assets/style.css").
-                                         */
-                                        private WebResourceResponse getCssWebResourceResponseFromAsset() {
-                                            try {
-                                                return getUtf8EncodedCssWebResourceResponse(getAssets().open("style.css"));
-                                            } catch (IOException e) {
-                                                return null;
-                                            }
-                                        }
-                                        /**
-                                         * Return WebResourceResponse with CSS markup from a raw resource (e.g. "raw/style.css").
-                                         */
-                            //            private WebResourceResponse getCssWebResourceResponseFromRawResource() {
-                            //                return getUtf8EncodedCssWebResourceResponse(getResources().openRawResource(R.raw.style));
-                            //            }
-                                        private WebResourceResponse getUtf8EncodedCssWebResourceResponse(InputStream data) {
-                                            return new WebResourceResponse("text/css", "UTF-8", data);
-                                        }
-
-        });
+        myWebView.setWebViewClient(new CustomBrowser());
         myWebView.getSettings().setJavaScriptEnabled(true);
-
         myWebView.getSettings().setDomStorageEnabled(true);
-
-
         myWebView.getSettings().setDatabaseEnabled(true);
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
             myWebView.getSettings().setDatabasePath(getCacheDir().getPath());
         }
-
-
-        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
         myWebView.loadUrl(indexUrl);
-
-        //  setContentView(R.layout.activity_web_browser);
     }
 }
